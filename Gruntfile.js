0e3b0f65e2d229724503f19400c723565f692008
xue.zhang <angelina.x.zhang@pwc.com>
2017-08-30 15:58:45 +0800

    test(datepickerPopup): debug CI test

--- Gruntfile.js
+++ Gruntfile.js
@@ -140,7 +140,7 @@ module.exports = function(grunt) {
         singleRun: true,
         autoWatch: false,
         reporters: ['dots'],
-        browsers: ['Firefox']
+        browsers: ['Chrome']
       },
       coverage: {
         preprocessors: {
